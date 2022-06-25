de10742253afe5d4ef8c16e801658acb17b29bdf
AliPort <bwithtacos@gmail.com>
2022-06-25 14:06:27 -0500

    2nd commit. Added JS.

---
+++ script.js
@@ -0,0 +1,25 @@
+window.onload = function (){
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    const word = // this will represent the word the player has to guess
+    const lives = // this will represent how many lives a player has left
+    const categories = // this will respresent the word categories in the game
+    const clue = // this will represent the clues that will help players guess the word
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
